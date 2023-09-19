9828eb494c719743d1b0235b6d054248ed268877
OTLOTLENG NKWADI <otlotleng.nkwadi@mmltd.co.za>
2023-09-19 11:55:39 +0200

    logout action and reducer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {distinctUntilChanged, map} from 'rxjs/operators';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 import { AppState } from './reducers';
 import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
+import { AuthActions } from './auth/action-types';
 
 @Component({
   selector: 'app-root',
@@ -52,7 +53,7 @@ export class AppComponent implements OnInit {
     }
 
     logout() {
-
+      this.store.dispatch(AuthActions.logout())
     }
 
 }
--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -25,6 +25,9 @@ export const authReducer = createReducer(
   initialAuthState,
   on(AuthActions.login, (state, action) => {
     return { user: action.user}
+  }),
+  on(AuthActions.logout, (state, action) => {
+    return { user: undefined}
   })
 );
 
